52ca347e922095548381445ceb47f13c49c63f51
Soil <kkkyj0220@naver.com>
2024-03-06 16:09:58 +0900

    3월6일 로그인이 핵심

--- servlet2/src/main/java/com/example/servlet2/AAAServlet.java
+++ servlet2/src/main/java/com/example/servlet2/AAAServlet.java
@@ -6,9 +6,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/aaa")
 public class AAAServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("data1", "요청 데이터입니다");
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("data2", "세션 데이터입니다");
+		
+		response.sendRedirect("/servlet2/bbb");
+		
+		// 여기서 forward를 한다면 jsp에서 data1, data2 모두 출력가능하다
+		// ${data1}, ${data2}
+		// jsp는 ${data1}이라고 하면 우선 request에서 찾는다 -> 있다 ->출력
+		// ${data2}의 경우 request에서 찾는다 -> 없다 -> 
+		// 그러면 session에서 찾는다
+		
+		// 세션유지시간
+		// session.setMaxInactiveInterval(3600);
+		// 세션 파괴 - 로그아웃 처리
+		// session.invalidate();
 	}
 }
---
+++ servlet2/src/main/java/com/example/servlet2/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.example.servlet2;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	// 로그인 화면을 보여준다
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd= request.getRequestDispatcher("로그인.jsp");
+		rd.forward(request, response);
+	}
+	
+	// 로그인을 처리
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 아이디가 spring, 비밀번호가 1234면 성공
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if(username.equals("spring")&&password.equals("1234")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("username", username);
+			response.sendRedirect("/servlet2/success");
+		} else {
+			response.sendRedirect("/servlet2/login");
+		}
+	}
+}
